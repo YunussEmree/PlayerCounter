71da8b4498128b7a06202a4feeec55e256631f1a
YunussEmree <senyigityunusemre@gmail.com>
2024-08-31 16:44:47 +0300

    Initial Commit

---
+++ src/main/java/com/YunussEmree/PlayerCounter/PlayerCounterApplication.java
@@ -0,0 +1,13 @@
+package com.YunussEmree.PlayerCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayerCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayerCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YunussEmree/PlayerCounter/PlayerCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YunussEmree.PlayerCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
